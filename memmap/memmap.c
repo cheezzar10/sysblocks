ff3069f0f458ac5a4265d590ab241612082d522c
Andrey Smirnov <andreysmirnov@odin.com>
2017-05-04 15:31:57 +0300

    ported to linux

--- memmap/memmap.c
+++ memmap/memmap.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
+/*
+  allocate two blocks of 128MB each
+  fill one of them with /dev/random
+  copy on to another
+ */
+
+#ifdef __linux__
+// required for pid_t
+#define __USE_XOPEN
+// required for MAP_ANON
+#define __USE_MISC
+#endif
+
+#include <unistd.h>
 #include <sys/mman.h>
 
 // clang -std=c99  -Wall -o memmap memmap.c
+// gcc -std=c99 -Wall -o memmap memmap.c
 
+const size_t MALLOC_BLKS = 8;
 const size_t MEM_BLK_SZ = 1024 * 1024;
+
+void print_bytes(const char* blk, size_t len);
 
 void create_mem_mapping() {
 	printf("creating new memory mapping\n");
@@ -17,7 +34,8 @@ void create_mem_mapping() {
 		exit(EXIT_FAILURE);
 	}
 	
-	printf("new memory mapping successfully created @%p\n", mem_blk_addr);
+	printf("new memory mapping successfully created @%p - content: ", mem_blk_addr);
+    print_bytes(mem_blk_addr, 8);
 	sleep(15);
 }
 
@@ -27,7 +45,7 @@ int main(int argc, char* argv[]) {
 	printf("memory allocator with pid=%d started.\n", pid);
 	
 	printf("memory allocation started\n");
-	for (int i=0;i<8;i++) {
+	for (int i = 0;i < MALLOC_BLKS;i++) {
 		void* mem_blk = malloc(MEM_BLK_SZ);
 		if (mem_blk == NULL) {
 			printf("memory block allocation failed\n");
@@ -36,7 +54,8 @@ int main(int argc, char* argv[]) {
 		}
 		
 		sleep(1);
-		printf("memory block allocated @%p\n", mem_blk);
+		printf("memory block allocated @%p - content: ", mem_blk);
+        print_bytes(mem_blk, 8);
 	}
 	printf("memory allocation completed\n");
 	
@@ -46,4 +65,14 @@ int main(int argc, char* argv[]) {
 	
 	printf("stopping memory allocator...\n");
 	return EXIT_SUCCESS;
+}
+
+void print_bytes(const char* blk, size_t len) {
+    if (len > 0) {
+        for (int i = 0;i < len;i++) {
+            printf("%02X ", blk[i]);
+        }
+    }
+    
+    printf("\n");
 }
