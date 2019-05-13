e54abf6eb2568a838fd4a84fe7d0854bb8e6f316
andrey <andrey@aurora.local>
2019-05-13 22:19:17 +0300

    code and data segment descriptors defined

--- tincanos/init.s
+++ tincanos/init.s
@@ -1,12 +1,17 @@
 .section .data
 
 # GDT definition
+# should be loaded on page boundary
 
 # GDT starts with null segment descriptor
 .fill 8
 
 # code segment descriptor
+
+# low 16 bits of segment limit
 .short 0xffff
+
+# low 24 bits of segment base
 .short 0x0000
 .byte  0x00
 
@@ -22,7 +27,22 @@
 .byte  0xcf
 .byte  0x00
 
+# data segment descriptor (will be also used as stack segment)
+
+.short 0xffff
+.short 0x0000
+.byte  0x00
+
+# 10010010
+.byte  0x92
+# 11001111
+.byte  0xcf
+
+# remaining bits of base
+.byte  0x00
+
+# interrupt vectors definition start
 
 .section .text
 
-# protected mode switching code will go here
+# here you can perform data and stack segment initialization and continue execution in pure 32 bit mode
