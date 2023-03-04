0fb071ad5cc1c037e5ef76d6a4b7cc0031d96635
Andrey Smirnov <andreysmirnov@ingrammicro.com>
2023-03-04 22:46:36 +0200

    extended boot loader experiments

--- bootldr/bootldr.asm
+++ bootldr/bootldr.asm
@@ -9,7 +9,7 @@ mov bp, sp
 
 ; reserving 512 bytes on stack
 ; for read buffer + space for locals
-sub sp, 210h
+sub sp, 21ch
 
 ; read buffer pointer
 mov bx, sp
@@ -22,29 +22,34 @@ mov cl, 1
 mov al, 1
 
 ; hdd 0
-mov dl, 80h
+mov dl, 81h
 
 int 13h
 
 ; reading first partition MBR offset in sectors
 mov bx, sp
 mov cx, [bx+01c6h]
-; partition 1 MBR offset
+; partition 1 MBR offset ( sector 0x3f )
 mov [bp-6], cx
 
 ; get disk 0 geometry
 mov ah, 08h
 int 13h
 
-; disk heads count
+; disk_heads = disc_head_max + 1 ( = 16 )
+inc dh
 mov [bp-1], dh
 
-; sectors per track count
+; sectors per track ( = 63 )
 mov al, cl
 and al, 3fh
 mov [bp-2], al
 
-; disk tracks count
+; sectors_per_cylinder = sectors_per_track x disk_heads ( === 1008 )
+mul dh
+mov [bp-8], ax
+
+; disk tracks max ( = 15 )
 mov ax, 0
 mov ah, cl
 and ah, c0h
@@ -60,10 +65,64 @@ mov [bp-4], ax
 ; number of root directory entries ( 2 bytes offset 0x11 === 512 ( 32 bytes each entry ) )
 ; number of FAT sectors ( 2 bytes offset 0x16 === 136 )
 
-; 0x2200 MBR
-; 0x2400 FAT
+; 0x7e00 MBR
 
-; 136 sectors per FAT x 512 x 2 FAT copies = 0x22000
+; track_number = ( offset / sectors_per_cylinder )
+mov ax, [bp-6]
+mov dx, 0
+mov cx, [bp-8]
+div cx
+
+; track_number = ax ( === 0 )
+mov [bp-0ah], ax
+
+; ( offset % sectors_per_cylinder ) / sectors_per_track
+mov ax, dx
+mov cl, [bp-2]
+div cl
+
+; head number = al
+; sector number = ah
+
+
+; read(head=al)
+mov dh, al
+
+; loading track number
+mov bx, [bp-0ah]
+mov cx, 8
+; shifting track number to bh
+shl bx, cl
+
+; read_sector
+mov cx, 0
+; read(sector=ah+1)
+mov cl, ah
+
+; sectors are counting from 1
+inc cx
+
+; read(track=bh)
+mov ch, bh
+
+; read(count=1) MBR is 512 bytes <=> 1 sector
+mov al, 1
+
+; read buffer pointer
+mov bx, sp
+
+; reading hdd 1
+mov dl, 81h
+
+; read function 02h
+mov ah, 02h
+
+int 13h
+
+; read directory offset and load it into memory ( allocate block on stack )
+; search for loader.sys and determine it's first cluster offset
+; also, read file size and how many iterations we need to read all it's clusters
+
 
 mov ah, 04ch
 mov al, cl
