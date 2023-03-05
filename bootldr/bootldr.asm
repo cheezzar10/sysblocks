a38b4f3603b7a1b9e1b9ec5723a615c026aa3f55
Andrey Smirnov <andreysmirnov@ingrammicro.com>
2023-03-05 17:25:55 +0200

    global sector offset to CHS conversion

--- bootldr/bootldr.asm
+++ bootldr/bootldr.asm
@@ -38,7 +38,7 @@ int 13h
 
 ; disk_heads = disc_head_max + 1 ( = 16 )
 inc dh
-mov [bp-1], dh
+mov [bp-DISK_HEADS_COUNT_VAR], dh
 
 ; sectors per track ( = 63 )
 mov al, cl
@@ -47,12 +47,12 @@ mov [bp-SECTORS_PER_TRACK_VAR], al
 
 ; sectors_per_cylinder = sectors_per_track x disk_heads ( === 1008 )
 mul dh
-mov [bp-8], ax
+mov [bp-SECTORS_PER_CYLINDER_VAR], ax
 
 ; disk tracks max ( = 15 )
 mov ax, 0
 mov ah, cl
-and ah, c0h
+and ah, 0c0h
 mov cl, 6
 shr ax, cl
 mov al, ch
@@ -66,27 +66,13 @@ mov [bp-4], ax
 
 ; track_number = ( offset / sectors_per_cylinder )
 mov ax, [bp-PARTITION_1_MBR_OFFSET_VAR]
-mov dx, 0
-mov cx, [bp-8]
-div cx
-
-; track_number = ax ( === 0 )
-mov [bp-0ah], ax
-
-; ( offset % sectors_per_cylinder ) / sectors_per_track
-mov ax, dx
-mov cl, [bp-SECTORS_PER_TRACK_VAR]
-div cl
-
-; head number = al
-; sector number = ah
-
+call get_chs
 
 ; read(head=al)
-mov dh, al
+mov dh, [chs_head]
 
 ; loading track number
-mov bx, [bp-0ah]
+mov bx, [chs_track]
 mov cx, 8
 ; shifting track number to bh
 shl bx, cl
@@ -94,15 +80,13 @@ shl bx, cl
 ; read_sector
 mov cx, 0
 ; read(sector=ah+1)
-mov cl, ah
-
-; sectors are counting from 1
+mov cl, [chs_sector]
 inc cx
 
 ; read(track=bh)
 mov ch, bh
 
-; read(count=1) MBR is 512 bytes <=> 1 sector
+; read(count=1) MBR is 512 bytes => 1 sector
 mov al, 1
 
 ; read buffer pointer
@@ -136,6 +120,9 @@ add ax, [bp-PARTITION_1_MBR_OFFSET_VAR]
 ; + MBR size = 1 sector
 inc ax
 
+; calculating root directory entries table CHS
+call get_chs
+
 ; number of root directory entries ( 2 bytes offset 0x11 === 512 ( 32 bytes each entry ) )
 mov cx, [si+ROOT_DIR_ENTRIES_COUNT]
 
@@ -147,7 +134,7 @@ shl dx, 1
 
 ; deallocating MBR buffer
 add sp, 200h
-; allocating root dicrectory table buffer
+; allocating root directory entries table buffer
 sub sp, dx
 
 ; searching for file with name KILL
@@ -163,9 +150,38 @@ mov ah, 04ch
 mov al, cl
 int 21h
 
+; get_chs(ax = global sector offset) returns result in global structure
+get_chs:
+mov dx, 0
+mov cx, [bp-SECTORS_PER_CYLINDER_VAR]
+div cx
+
+; saving track number
+mov [chs_track], ax
+
+; ( offset % sectors_per_cylinder ) / sectors_per_track
+mov ax, dx
+mov cl, [bp-SECTORS_PER_TRACK_VAR]
+div cl
+
+; head number = al
+mov [chs_head], al
+; sector number = ah
+mov [chs_sector], ah
+
+ret
+
 [section .data]
+; get_chs function return value will be stored here 01C0
+chs_track dw 0
+chs_head db 0
+chs_sector db 0
+
+; constants
 PARTITION_1_MBR_OFFSET_VAR equ 6
 SECTORS_PER_TRACK_VAR equ 2
+DISK_HEADS_COUNT_VAR equ 1
+SECTORS_PER_CYLINDER_VAR equ 8
 READ_SECTORS equ 02h
 HDD_1 equ 81h
 GET_DRIVE_PARAMETERS equ 08h
